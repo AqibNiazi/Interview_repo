d70deefcfb173e52e16bc70c5655051bb58665e8
AqibNiazi <aqibjaved5201@gmail.com>
2024-05-15 23:59:53 +0500

    reverse integer

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -7,10 +7,10 @@
 //   reverseInt(500) === 5
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
-
+// Solution
 function reverseInt(n) {
   return parseInt(n.toString().split("").reverse().join("")) * Math.sign(n);
 }
 console.log(reverseInt(-23));
 
-module.exports = reverseInt;
+
