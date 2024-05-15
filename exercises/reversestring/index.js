89a4bbbed29b8823a8c03954d530f3d2a1f5aa60
AqibNiazi <aqibjaved5201@gmail.com>
2024-05-15 16:07:45 +0500

    string reversal problem with 4 solutions completed

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,35 +2,48 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-// My first solution with for loop
-
-// function reverse(str) {
-//   let reverseString = "";
-//   for (let i = str.length - 1; i >= 0; i--) {
-//     reverseString += str[i];
+////Solution 1 with Classic for loop
+// function reverseString(str) {
+//     let reversedString="";
+//   for (let i = str.length-1; i >= 0; i--) {
+//      reversedString += str[i]
+    
 //   }
-//   return reverseString;
+//     return reverseString
 // }
+// console.log(reverseString("apple"));
+//////////Solution 2 With Array methods
 
-// console.log(reverse("apple"));
+// const reverseString = (str) => str.split('').reverse().join('')
+// console.log(reverseString("apple"));
 
-//my second solution
-// const reverse = (str) => str.split("").reverse().join("");
-// console.log(reverse("apple"));
+// Solution 3 with for of loop
 
 // const reverse = (str) => {
 //   let reverseString = "";
 //   for (let char of str) {
 //     reverseString = char + reverseString;
+
 //   }
 //   return reverseString;
 // };
 // console.log(reverse("apple"));
-//my fourth solution
-const reverse = (str) => str.split("").reduce((rev, char) => (char += rev), "");
+/*
+Explanation of SOlution 3
+
+Initial reverseString: ""
+Iteration 1: char = "a", reverseString = "a" + "" → reverseString = "a"
+Iteration 2: char = "p", reverseString = "p" + "a" → reverseString = "pa"
+Iteration 3: char = "p", reverseString = "p" + "pa" → reverseString = "ppa"
+Iteration 4: char = "l", reverseString = "l" + "ppa" → reverseString = "lppa"
+Iteration 5: char = "e", reverseString = "e" + "lppa" → reverseString = "elppa"
+*/
+
+//4th solution with reduce method
+const reverse = (str) => str.split('').reduce((reversed,character)=>character+reversed,'')
 console.log(reverse("apple"));
-module.exports = reverse;
+
