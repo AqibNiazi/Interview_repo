72f89bcb4e2d60caa6b356a2ff87ecaf0c7ec365
AqibNiazi <aqibjaved5201@gmail.com>
2025-07-17 23:30:44 +0500

    string reversel solution posted

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,16 +6,22 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+
+
 ////Solution 1 with Classic for loop
+
 // function reverseString(str) {
-//     let reversedString="";
-//   for (let i = str.length-1; i >= 0; i--) {
-//      reversedString += str[i]
-    
+//   let reverseStr = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reverseStr += str[i];
 //   }
-//     return reverseString
+//   return reverseStr;
 // }
 // console.log(reverseString("apple"));
+ 
+
+
+
 //////////Solution 2 With Array methods
 
 // const reverseString = (str) => str.split('').reverse().join('')
@@ -23,15 +29,15 @@
 
 // Solution 3 with for of loop
 
-// const reverse = (str) => {
-//   let reverseString = "";
-//   for (let char of str) {
-//     reverseString = char + reverseString;
+const reverse = (str) => {
+  let reverseString = "";
+  for (let char of str) {
+    reverseString = char+reverseString;
 
-//   }
-//   return reverseString;
-// };
-// console.log(reverse("apple"));
+  }
+  return reverseString;
+};
+console.log(reverse("apple"));
 /*
 Explanation of SOlution 3
 
@@ -44,6 +50,6 @@ Iteration 5: char = "e", reverseString = "e" + "lppa" → reverseString = "elppa
 */
 
 //4th solution with reduce method
-const reverse = (str) => str.split('').reduce((reversed,character)=>character+reversed,'')
-console.log(reverse("apple"));
+// const reverse = (str) => str.split('').reduce((reversed,character)=>character+reversed,'')
+// console.log(reverse("apple"));
 
