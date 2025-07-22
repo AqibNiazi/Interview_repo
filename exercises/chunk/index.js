339ecfc158caff0c991a3904c3b688b5597e8371
AqibNiazi <aqibjaved5201@gmail.com>
2025-07-22 12:04:16 +0500

    Array chunking problem solved

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,23 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  // create empty array to hold chunks called chunked array
+  const chunked = [];
+  // for each element in the unchunked array retrive the last element
+  for (const element of array) {
+    const last = chunked[chunked.length - 1];
+    //if last element doesn't exist or it's length is equal to chunk size
+    if (!last || last.length === size) {
+      chunked.push([element]);
+    } else {
+      // add current element into the chunk
+      last.push(element);
+    }
+  }
+  return chunked;
+}
+
+console.log(chunk([1, 2, 3, 4], 2));
 
 module.exports = chunk;
