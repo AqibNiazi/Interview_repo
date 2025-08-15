68a4219fab57a4a32ef7dd76704d212e566312f6
AqibNiazi <aqibjaved5201@gmail.com>
2025-08-15 22:42:46 +0500

    Capitalized second solution added

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -6,20 +6,39 @@
 //   capitalize('a short sentence') --> 'A Short Sentence'
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
+// Solution 1
+// function capitalize(str) {
+//   // first create an array to store capitized words.
+//   const words = [];
+
+//   // Loop through the array to make capitalized words from the array.
+
+//   for (let word of str.split(" ")) {
+//     //first capitalize first letter of each word in the array and then combine it with other part of the word using slice method.
+//     words.push(word[0].toUpperCase() + word.slice(1));
+//   }
+//   //change all the words into string using join method and return it.
+//   return words.join(" ");
+// }
+
+// console.log(capitalize("hello hi there"));
+
+
+// Solution 2:
 
 function capitalize(str) {
-  // first create an array to store capitized words.
-  const words = [];
+  //create a result string with the first letter capitalized
+  let result=str[0].toUpperCase();
 
-  // Loop through the array to make capitalized words from the array.
-
-  for (let word of str.split(" ")) {
-    //first capitalize first letter of each word in the array and then combine it with other part of the word using slice method.
-    words.push(word[0].toUpperCase() + word.slice(1));
+  for (let i = 1; i < str.length; i++) {
+    
+      if(str[i-1]===' '){
+        result+=str[i].toUpperCase()
+      }else{
+        result+=str[i]
+      }
   }
-  //change all the words into string using join method and return it.
-  return words.join(" ");
+  return result
 }
 
-console.log(capitalize("hello hi there"));
-
+console.log(capitalize("hello wordls"));
